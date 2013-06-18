2c38c0ce25f1a797c6daf69894af253b4e76c1ef
Daniel Silverman <me@agperson.com>
2013-06-18 11:44:02 -0400

    adding variables

--- site/roles/manifests/base.pp
+++ site/roles/manifests/base.pp
@@ -3,7 +3,9 @@ class roles::base {
   include base::users
 
   class { 'puppet::agent':
-    server => 'puppetmaster.oo.huit.harvard.edu',
-    method => 'service',
+    server        => 'puppetmaster.oo.huit.harvard.edu',
+    ca_server     => 'puppetmaster.oo.huit.harvard.edu',
+    report_server => 'puppetmaster.oo.huit.harvard.edu',
+    method        => 'service',
   }
 }
