166f8f8bf3c008abe3ddfef35a9670c8196cc27d
Daniel Silverman <me@agperson.com>
2013-06-20 07:22:11 -0400

    Puppetmaster bootstrap script Script to bootstrap a new Puppetmaster, as well as minor improvements to ec2::mgmt

--- dist/ec2/manifests/mgmt.pp
+++ dist/ec2/manifests/mgmt.pp
@@ -1,27 +1,29 @@
-class ec2::mgmt extends ec2 {
-  # Dependencies for puppet cloud provisioner
-  $pkgs = [ 'ruby-devel', 'gcc', 'rubygems', 'libxml2-devel', 'libxslt-devel', 'rubygems-json' ]
-  $gems = [ 'guid', 'json', 'nokogiri' ]
-  package { $pkgs: }
-  package { $gems: 
+class ec2::mgmt inherits ec2 {
+  # Dependencies for puppet cloud provisioner and instance creation script
+  $pkgs = [ 'ruby-devel', 'gcc', 'rubygems', 'libxml2-devel', 'libxslt-devel',
+            'rubygems-json' ]
+  $gems = [ 'guid', 'json', 'trollop', 'colored']
+  package { $pkgs:
+    ensure => installed,
+  }
+
+  package { $gems:
     ensure   => installed,
     require  => Package[$pkgs],
-    provider => gem,
-  }
-
-  Package['nokogiri'] {
-    version => '1.5.10',
-  }
-
-  # Instance creation script
-  package { [ 'trollop', 'colored' ]:
-    ensure   => 'installed',
     provider => 'gem',
   }
 
+  package { 'nokogiri':
+    ensure   => '1.5.10',
+    require  => Package[$pkgs],
+    provider => 'gem',
+  }
+
+  # Instance creation script
   file { '/usr/sbin/oo-create-instance':
     owner  => 'root',
     group  => 'root',
     mode   => '0700',
     source => 'puppet:///dist/ec2/usr/sbin/oo-create-instance',
+  }
 }
---
+++ puppetmaster-bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Bootstrap new Puppetmaster
+# - Create a new m1.small instance of type ami-52009e3b
+# - SCP this script and run it as root
+
+rpm -i http://yum.puppetlabs.com/el/6Server/products/x86_64/puppetlabs-release-6-7.noarch.rpm
+yum install git rubygems ruby-devel gcc libxml2-devel libxslt-devel puppet
+gem install librarian-puppet
+git clone http://github.com/huit/openshift-poc.git
+cd openshift-poc
+librarian-puppet install
+puppet apply -v --modulepath=dist:modules:site -e 'include profiles::openshift::puppetmaster'
